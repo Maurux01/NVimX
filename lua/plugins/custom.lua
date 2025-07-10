f75eb5226063d3934a7ce4d537b60bc64afbdb4c
maurux01 <maurux01@gmail.com>
2025-07-10 09:21:45 -0500

    Add cmp configuration and setup for LSP completion

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -934,6 +934,18 @@ return {
     opts = {},
   },
 }
+-- ...existing code...
+use {
+  'hrsh7th/nvim-cmp',
+  requires = {
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+    'hrsh7th/cmp-cmdline',
+    'L3MON4D3/LuaSnip'
+  }
+}
+-- ...existing code...
 
 -- Filtrar notificaciones de healthcheck de which-key
 if pcall(require, "notify") then
